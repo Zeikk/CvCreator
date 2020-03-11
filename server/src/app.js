9b29953f8cc446f60a54f5114163b999da084861
loickl <44809393+loickl@users.noreply.github.com>
2020-03-11 16:39:23 +0100

    server

--- server/src/app.js
+++ server/src/app.js
@@ -6,12 +6,17 @@ const cvRouter = require('./routers/cv')
 
 const app = express()
 
+app.set("view engine", "ejs");
+
 // Website under Maintenance
 // app.use((req, res, next) => {
 // 	res.status(503).send('We are currently under maintenance')
 // })
 app.use(cors())
 app.use(express.json())
+app.use('/', (req, res) => {
+render('./pages/index.ejs')
+})
 app.use(userRouter)
 app.use(cvRouter)
 
